66c50e13b2ee9f641ab628c1f10924543edfbda9
Anshul Bhatt <134294749+Anshul123B@users.noreply.github.com>
2023-11-11 19:37:15 +0530

    Add files via upload

---
+++ default_constructer.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+class Line
+{
+	public:
+		int size;
+		
+	Line()
+	{
+		size=30;
+	}
+};
+
+int main()
+{
+	Line l;
+	cout<<"Size of line is: "<<" "<<"cm";
+	return 0;
+}
