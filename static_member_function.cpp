5a33859fe3dbda2205dbba1af6b43cea29927313
Anshul Bhatt <134294749+Anshul123B@users.noreply.github.com>
2023-12-05 22:08:31 +0530

    Add files via upload

---
+++ static_member_function.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+class demo
+{
+	int x,y;
+	static int z;
+	public:
+		void getdata()
+		{
+			cout<<"Enter two numbers: ";
+			cin>>x,y;
+		}
+		void showdata()
+		{
+		  cout<<"\nx= "<<x<<"\ty= "<<y;
+		  cout<<"\nZ= "<<z;
+		}
+		static void print()
+		{
+			cout<<"Static variable: "<<z;
+		}
+};
+int demo::z;
+int main()
+{
+	demo aa,bb;
+	aa.getdata();
+	bb.getdata();
+	aa.showdata();
+	bb.showdata();
+	return 0;
+ } 
